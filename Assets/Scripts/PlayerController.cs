9f513c6ddb3f0fcf911822397f6c1ebeb798811a
nburner <nburner@harding.edu>
2018-01-29 01:40:12 -0600

    Initial Commit Into Organization

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speedyHurryUpZoom;
+    public Text countText;
+    public Text endText;
+
+    private Rigidbody rb;
+    private int count;
+    private float elapsedTime;
+    private bool gameOver;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        endText.text = "";
+        elapsedTime = 0.0f;
+        gameOver = false;
+    }
+
+    void Update()
+    {
+        if (!gameOver)
+        {
+
+        }
+
+        // Did user press 'N'?
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            NewGame();
+        }
+    }
+
+    private void NewGame()
+    {
+        print("NewGame called.");
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        foreach (var go in allObjects)
+        {
+            if (go.CompareTag("Pick Up") || go.CompareTag("Pick Up Special"))
+            {
+                go.SetActive(true);
+            }
+        }
+        endText.text = "";
+        GameObject.Find("Player").SendMessage("Finish");
+    }
+
+    // Called before any Physics Calculations are performed
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticalal = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVerticalal);
+        rb.AddForce(movement * speedyHurryUpZoom);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {        
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            ++count;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Pick Up Special"))
+        {
+            other.gameObject.SetActive(false);
+            count += 5;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 16)
+        {
+            endText.text = "You Win!";
+            GameObject.Find("Player").SendMessage("Finish");
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public Text endText;
+    private float timer;
+    private bool finished = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timer = 30;
+        timerText.text = "Time: 30";
+        InvokeRepeating("UpdateTimer", 1.0f, 1.0f);
+	}
+	
+	void UpdateTimer ()
+    {
+        if (finished)
+        {
+            return;
+        }
+
+        float t = timer - Time.time;
+        string seconds = (t % 60).ToString("f0");
+        timerText.text = "Time: " + seconds;
+
+        if (t <= 0.0f)
+        {
+            finished = true;
+            GameObject[] allObjects = FindObjectsOfType<GameObject>();
+            foreach (var go in allObjects)
+            {
+                if (go.CompareTag("Pick Up") || go.CompareTag("Pick Up Special"))
+                {
+                    go.SetActive(false);
+                }
+            }
+            endText.text = "Out of Time! You Lose.";
+        }
+	}
+
+    public void Finish()
+    {
+        finished = true;
+        timerText.color = Color.yellow;
+    }
+}
