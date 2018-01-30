e3f72d62c03ac2dcb417b3976aaf8a51a228d2ee
nburner <nburner@harding.edu>
2018-01-30 01:24:26 -0600

    Implemented Timer in PlayerController
    Sacrificed separation of concerns for simplicity by removing the Timer
    class and altering its implementation to work in the PlayerController
    class.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,56 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speedyHurryUpZoom;
+    public float speed;
     public Text countText;
     public Text endText;
+    public Text timerText;
 
+    private System.Timers.Timer timer;
     private Rigidbody rb;
-    private int count;
-    private float elapsedTime;
-    private bool gameOver;
+    private List<GameObject> pickUps;
+    private int score;
+    private int scoreToWin = 16;
+    // Integer that holds the initial time
+    private const int startTime = 30;
+    // Integer to decrement and display to player
+    private int timeTick;
 
     void Start()
     {
+        pickUps = new List<GameObject>();
         rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText();
+        score = 0;
+        timeTick = startTime;
+        countText.text = "Count: " + score.ToString();
         endText.text = "";
-        elapsedTime = 0.0f;
-        gameOver = false;
+        timerText.text = "Time: " + timeTick;
+
+        PopulatePickUpsList();
+        // Begin timer. Wait one second to start timer to ensure player is ready.
+        InvokeRepeating("TimerTick", 1f, 1f);       
+    }
+
+    // Populate pickUps List with all PickUp prefabs
+    private void PopulatePickUpsList()
+    {
+        var goList = FindObjectsOfType<GameObject>();
+        foreach(var go in goList)
+        {
+            if (go.CompareTag("Pick Up") || go.CompareTag("Pick Up Special"))
+            {
+                pickUps.Add(go);
+            }
+        }
     }
 
     void Update()
     {
-        if (!gameOver)
-        {
-
-        }
+        CheckWin();        
 
         // Did user press 'N'?
         if (Input.GetKeyDown(KeyCode.N))
@@ -39,19 +59,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void CheckWin()
+    {
+        if (timeTick <= 0)
+        {
+            CancelInvoke("TimerTick");
+            foreach (var go in pickUps)
+            {
+                go.SetActive(false);
+            }
+            endText.text = "Out of Time! You Lose.";
+        }
+        else if (score == scoreToWin)
+        {
+            CancelInvoke("TimerTick");
+            endText.text = "You won! And with " + timeTick.ToString() + " seconds to spare. ";
+            endText.text += timeTick > 5 ? "Impressive!" : "That was close!";
+        }
+    }
+
     private void NewGame()
     {
-        print("NewGame called.");
-        GameObject[] allObjects = FindObjectsOfType<GameObject>();
-        foreach (var go in allObjects)
+        CancelInvoke("TimerTick");
+        foreach (var go in pickUps)
         {
-            if (go.CompareTag("Pick Up") || go.CompareTag("Pick Up Special"))
-            {
-                go.SetActive(true);
-            }
+            go.SetActive(true);
         }
+        score = 0;
+        timeTick = startTime;
+        countText.text = "Count: " + score.ToString();
+        timerText.text = "Time: " + timeTick.ToString();
         endText.text = "";
-        GameObject.Find("Player").SendMessage("Finish");
+        InvokeRepeating("TimerTick", 1f, 1f);
     }
 
     // Called before any Physics Calculations are performed
@@ -61,7 +100,7 @@ public class PlayerController : MonoBehaviour
         float moveVerticalal = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVerticalal);
-        rb.AddForce(movement * speedyHurryUpZoom);
+        rb.AddForce(movement * speed);
     }
 
     void OnTriggerEnter(Collider other)
@@ -69,24 +108,30 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
-            ++count;
-            SetCountText();
+            ++score;
         }
         else if (other.gameObject.CompareTag("Pick Up Special"))
         {
             other.gameObject.SetActive(false);
-            count += 5;
-            SetCountText();
+            score += 5;
         }
+
+        countText.text = "Count: " + score.ToString();
     }
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 16)
+        countText.text = "Count: " + score.ToString();
+        if (score >= 16)
         {
             endText.text = "You Win!";
             GameObject.Find("Player").SendMessage("Finish");
         }
+    }
+
+    void TimerTick()
+    {
+        --timeTick;
+        timerText.text = "Time: " + timeTick;
     }
 }
